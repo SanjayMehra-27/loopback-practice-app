d7b0fa12fdebea8b42741dab51cdc172abdb4e97
Sanjay Mehra <mehra271198@gmail.com>
2022-02-05 21:28:19 +0530

    Update ping.controller.ts

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -43,7 +43,7 @@ export class PingController {
   ping(): object {
     // Reply with a greeting, the current time, the url, and request headers
     return {
-      greeting: 'Hello from Sanjay Mehra',
+      greeting: 'Hello from First Ping API',
       date: new Date(),
       url: this.req.url,
       headers: Object.assign({}, this.req.headers),
